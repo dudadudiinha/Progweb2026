71549141a90e1952ff8ec2d8b1b6528994176bc4
dudadudiinha <moura.nascimento1@escolar.ifrn.edu.br>
2026-05-04 11:22:27 -0300

    Atualização rápida

--- loja/loja/views/ProdutoView.py
+++ loja/loja/views/ProdutoView.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
 
 def list_produto_view(request, id=None):
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+    if id is None:
+        return HttpResponse('<h1>Nenhum id foi informado</h1>')
