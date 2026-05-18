0b5573d4ce28cc7cf6ca8e8ae59fdfccb1abb3ed
dudadudiinha <moura.nascimento1@escolar.ifrn.edu.br>
2026-05-18 11:38:32 -0300

    Produto no terminal

--- loja/loja/views/ProdutoView.py
+++ loja/loja/views/ProdutoView.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from loja.models import Produto
+from datetime import timedelta, datetime
+from django.utils import timezone
 
 def list_produto_view(request, id=None):
     produto = request.GET.get("produto")
@@ -7,18 +9,24 @@ def list_produto_view(request, id=None):
     promocao = request.GET.get("promocao")
     categoria = request.GET.get("categoria")
     fabricante = request.GET.get("fabricante")
-    produtos = Produto.objects.filter(Produto=produto)
-    print(produtos)
-    if destaque is not None:
-        print(destaque)
+    dias = request.GET.get("dias")
+    produtos = Produto.objects.all()
+
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)   
     if produto is not None:
-        print(produto)
+        produtos = produtos.filter(Produto__contains=produto )
     if promocao is not None:
-        print(promocao)
+        produtos = produtos.filter(promocao=promocao)
+    if destaque is not None:
+        produtos = produtos.filter(destaque=destaque)
     if categoria is not None:
-        print(categoria)
+        produtos = produtos.filter(categoria__Categoria=categoria)
     if fabricante is not None:
-        print(fabricante)
-    if id is None:
-        id = 0
+        produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    print(produtos)
     return HttpResponse('<h1>Produto de id %s!</h1>' % id)
